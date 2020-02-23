f874c761a7fcd1e9c5c4360599592a1a154f945f
LuK1337 <priv.luk@gmail.com>
2020-02-23 10:19:38 +0100

    kirin: Load kernel modules after unmounting /vendor
    Change-Id: I19b1d6874afeb5555725cfe390397c3d778857a6

--- recovery/root/sbin/load_kernel_modules.sh
+++ recovery/root/sbin/load_kernel_modules.sh
@@ -3,5 +3,5 @@
 SLOT=`getprop ro.boot.slot_suffix`
 mount /dev/block/bootdevice/by-name/vendor$SLOT /vendor -o ro
 cp /vendor/lib/modules/nt36xxx_driver.ko /sbin
-insmod /sbin/nt36xxx_driver.ko &
 umount /vendor
+insmod /sbin/nt36xxx_driver.ko
