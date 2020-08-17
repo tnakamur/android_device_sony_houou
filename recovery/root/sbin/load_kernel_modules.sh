4dabb12315284138bca0e7305a11a3979afb69c3
tnakamur <taka.nakamura1009@gmail.com>
2020-08-17 09:06:13 +0900

    houou: Load touchscreen driver module if needed * Needed for stock kernel.

--- recovery/root/sbin/load_kernel_modules.sh
+++ recovery/root/sbin/load_kernel_modules.sh
@@ -2,6 +2,26 @@
 
 SLOT=`getprop ro.boot.slot_suffix`
 mount /dev/block/bootdevice/by-name/vendor$SLOT /vendor -o ro
-cp /vendor/lib/modules/nt36xxx_driver.ko /sbin
+cp /vendor/lib/modules/clearpad_core.ko /sbin
+cp /vendor/lib/modules/clearpad_i2c.ko /sbin
+cp /vendor/lib/modules/clearpad_rmi_dev.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_core.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_device.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_diagnostics.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_i2c.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_recovery.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_reflash.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_testing.ko /sbin
+cp /vendor/lib/modules/synaptics_tcm_touch.ko /sbin
 umount /vendor
-insmod /sbin/nt36xxx_driver.ko
+insmod /sbin/clearpad_core.ko
+insmod /sbin/clearpad_i2c.ko
+insmod /sbin/clearpad_rmi_dev.ko
+insmod /sbin/synaptics_tcm_core.ko
+insmod /sbin/synaptics_tcm_device.ko
+insmod /sbin/synaptics_tcm_diagnostics.ko
+insmod /sbin/synaptics_tcm_i2c.ko
+insmod /sbin/synaptics_tcm_recovery.ko
+insmod /sbin/synaptics_tcm_reflash.ko
+insmod /sbin/synaptics_tcm_testing.ko
+insmod /sbin/synaptics_tcm_touch.ko
